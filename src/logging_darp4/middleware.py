6189895820559fdbea63b9cb79cc15b735ddfe7b
Erick Ruiz <eruisacosta@gmail.com>
2026-07-15 19:55:40 -0500

    Quitamos librerias no utilizadas

--- src/logging_darp4/middleware.py
+++ src/logging_darp4/middleware.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
-from starlette.responses import JSONResponse, Response
+from starlette.responses import Response
 
 logger = logging.getLogger("request")
 
